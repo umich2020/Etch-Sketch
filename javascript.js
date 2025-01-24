252502ea2d3ca8311d1c791071342be5f93f9426
umich2020 <jimson@umich.com>
2025-01-23 23:30:57 -0500

    Changing size of squares
    I manage to change classes but there's bug issues when i'm trying
    to change my class, it's not selecting all the non filled ones
    I'm thinking about adding an id or figuring out the order of the class

--- javascript.js
+++ javascript.js
@@ -7,10 +7,20 @@ for (let i=0; i<16*16; i++) {
     listen(box)
 
 }
+const change = document.querySelector("#change")
+change.addEventListener("click",remove)
+
+//functions
 function listen(square) {
     square.addEventListener("mouseenter", () => {
-        square.classList.add("filled")
-        square.classList.remove("square")
+        square.classList.toggle("filled")
+        square.classList.toggle("square")
         console.log("this is working")
+    },{once: true})
+}
+function remove() {
+    const squares = document.querySelectorAll(".square")
+    squares.forEach((square)=> {
+    container.removeChild(square)
     })
 }
