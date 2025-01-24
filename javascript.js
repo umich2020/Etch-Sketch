af6f78366cda264e3743da9a329614365e92df04
umich2020 <jimson@umich.com>
2025-01-24 16:24:31 -0500

    finished everything except extra credit

--- javascript.js
+++ javascript.js
@@ -3,14 +3,32 @@ for (let i=0; i<16*16; i++) {
     const box = document.createElement("div")
     box.classList.add("square")
     container.appendChild(box)
-    box.textContent="box"
     listen(box)
 
 }
 const change = document.querySelector("#change")
-change.addEventListener("click",remove)
+change.addEventListener("click", () =>{
+    remove()
+    let userInput = prompt("this is a test message",16)
+    if (userInput <= 100 && userInput>=1) {
+        createSquares(userInput)
+    }
+})
 
 //functions
+function createSquares(size) {
+    let pxSize = 480/size + "px"
+    console.log(pxSize)
+    for (let i=0; i<size*size; i++) {
+        const box = document.createElement("div")
+        box.classList.add("square")
+        container.appendChild(box)
+        listen(box)
+        document.getElementsByClassName("square")[i].style.width = pxSize
+        document.getElementsByClassName("square")[i].style.height = pxSize
+
+    }
+}
 function listen(square) {
     square.addEventListener("mouseenter", () => {
         square.classList.add("filled")
